c47b2c7d021cee039bd478fe861aeb3566ee9ad7
Manawyrm <git@tbspace.de>
2023-10-08 16:44:07 +0000

    amrnbcodec/efrcodec: switch from ETSI amrnb to libopencore-amrnb

--- modules/amrnbcodec.cpp
+++ modules/amrnbcodec.cpp
@@ -25,12 +25,21 @@
 #include <yatephone.h>
 
 extern "C" {
-#include <interf_enc.h>
-#include <interf_dec.h>
+#include <opencore-amrnb/interf_enc.h>
+#include <opencore-amrnb/interf_dec.h>
 }
 namespace RxTypes {
-// There is a conflict between encoder and decoder so insulate in a namespace
-#include <sp_dec.h>
+	/* Declaration recieved frame types */
+	enum RXFrameType { RX_SPEECH_GOOD = 0,
+	                   RX_SPEECH_DEGRADED,
+	                   RX_ONSET,
+	                   RX_SPEECH_BAD,
+	                   RX_SID_FIRST,
+	                   RX_SID_UPDATE,
+	                   RX_SID_BAD,
+	                   RX_NO_DATA,
+	                   RX_N_FRAMETYPES     /* number of frame types */
+	};
 };
 
 // IF1/GP3 is Bandwidth-Efficient Mode
